26beab1eb20644427349adde95ba227365dcd44f
Holly Corbin <holly@H-Mac.local>
2022-07-29 10:05:44 -0400

    added annotations to customer class

--- project-two-hotel/src/main/java/com/skillstorm/hotel/models/Customers.java
+++ project-two-hotel/src/main/java/com/skillstorm/hotel/models/Customers.java
@@ -1,17 +1,49 @@
 package com.skillstorm.hotel.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+@Entity
+@Table(name="customers")
 public class Customers {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="customer_id")
 	private int customerId;
+	
+	@NotBlank
+	@Column(name="first_name")
 	private String firstName;
+	
+	@NotBlank
+	@Column(name="last_name")
 	private String lastName;
+	
+	@NotBlank
+	@Email
+	@Column(name="email")
 	private String email;
-	private int phoneNumber;
+	
+	@Column(name="phone_number")
+	@Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$")
+	private String phoneNumber;
+	
+	@Positive
+	@Column(name="num_guests")
 	private int numGuests;
 	
 	public Customers() {}
 
-	public Customers(int customerId, String firstName, String lastName, String email, int phoneNumber, int numGuests) {
+	public Customers(int customerId, String firstName, String lastName, String email, String phoneNumber, int numGuests) {
 		super();
 		this.customerId = customerId;
 		this.firstName = firstName;
@@ -21,7 +53,7 @@ public class Customers {
 		this.numGuests = numGuests;
 	}
 
-	public Customers(String firstName, String lastName, String email, int phoneNumber, int numGuests) {
+	public Customers(String firstName, String lastName, String email, String phoneNumber, int numGuests) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -62,11 +94,11 @@ public class Customers {
 		this.email = email;
 	}
 
-	public int getPhoneNumber() {
+	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
-	public void setPhoneNumber(int phoneNumber) {
+	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
